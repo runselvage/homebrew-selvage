7cf19e01a6fb1b490d0e9da3e9372d2ac12605b6
Venkat Podugu <venkat@q3learners.com>
2026-08-30 22:20:18 -0400

    selvage 0.2.22: fix .selvage transport-file false-completion guard

--- Formula/selvage.rb
+++ Formula/selvage.rb
@@ -5,21 +5,21 @@ class Selvage < Formula
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/runselvage/selvage/releases/download/v0.2.21/selvage-darwin-arm64.tar.gz"
-      sha256 "d5f0f6bb19cfeb6d1bac22584f1a9e71c5c4978fdbaa947ca7b6065048107ded"
+      url "https://github.com/runselvage/selvage/releases/download/v0.2.22/selvage-darwin-arm64.tar.gz"
+      sha256 "1794620e20be9597422132f2d8b340dc50bb64aa746195ca2ab8efc4c11fbd2b"
     else
-      url "https://github.com/runselvage/selvage/releases/download/v0.2.21/selvage-darwin-amd64.tar.gz"
-      sha256 "822f4ec221372634e03db22399896a96b3af35a3197ad31b4a073963f7b0ca4e"
+      url "https://github.com/runselvage/selvage/releases/download/v0.2.22/selvage-darwin-amd64.tar.gz"
+      sha256 "25614394efbb327405d32a7c1d1b09ec98b09f4cf79bbbd71fa9142124af406d"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/runselvage/selvage/releases/download/v0.2.21/selvage-linux-arm64.tar.gz"
-      sha256 "ec444ff66a6a70b39cba5c73ab1bfc2953c9cc8a4a0647875a71f2d14ca56f61"
+      url "https://github.com/runselvage/selvage/releases/download/v0.2.22/selvage-linux-arm64.tar.gz"
+      sha256 "ae98c0072e2928436eb573d134c4366463a396c665a6bc58a2c884eadacfcc9c"
     else
-      url "https://github.com/runselvage/selvage/releases/download/v0.2.21/selvage-linux-amd64.tar.gz"
-      sha256 "3dc08bc30c9bbe4ad45d30f60d94bd88686fbdfce6b31772909271c1afd7521d"
+      url "https://github.com/runselvage/selvage/releases/download/v0.2.22/selvage-linux-amd64.tar.gz"
+      sha256 "25cef6c7eefdba61172774faa74c67165244928e02e774f2f0c626a81a1e4dba"
     end
   end
 
